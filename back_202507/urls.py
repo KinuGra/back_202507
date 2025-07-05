266e011a2a40cdae2f871f3c7eaa2158ff7cb529
gear_kinu <techgit1024@gmail.com>
2025-07-06 01:13:03 +0900

    chore: DRFでAPIを作るテスト

--- back_202507/urls.py
+++ back_202507/urls.py
@@ -14,10 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.contrib import admin # Djangoの管理サイト機能を利用するためのモジュールをインポート
+from django.urls import path, include # URLパスを定義するためのpath関数と、他のURLconfを参照するためのinclude関数をインポート
+from rest_framework.routers import DefaultRouter # DRFから、デフォルトのルータークラスDefaultRouterをインポート。ViewSetとURLの自動マッピングを可能にする
+from main.views import ItemViewSet # api/views.pyからItemViewSetクラスをインポート
+
+# DefaultRouterのインスタンスを作成
+router = DefaultRouter()
+# ItemViewSetをitemsパスでルーターに登録している。これにより、ItemViewSetに定義された操作に基づいて、自動的にURLが生成される
+router.register(r'items', ItemViewSet)
 
 urlpatterns = [
+    # Django管理サイトへのpath
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+
+    # routerで生成されたすべてのURLを/api/パス以下に含めるように定義。
+    # これにより、ItemViewSetの操作に対応するURLが/api/items/のようにアクセスできるようにする
+    path('api/', include(router.urls))
 ]
