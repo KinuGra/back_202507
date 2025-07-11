24c9e3df8a2909dceec156cefd0fbd942d69c161
gear_kinu <techgit1024@gmail.com>
2025-07-12 02:48:47 +0900

    add: ルームが存在するかどうかの照合API
    DBのカラム修正

--- main/views.py
+++ main/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.http import require_GET
 from rest_framework import viewsets # DRFからviewsetsをインポート。ウェブページの表示やデータ処理などの一連の操作をひとまとめにしたクラス（=ビューセット）
 from .models import Item, Room, QuizData1 # 同じディレクトリにあるmodels.pyファイルからItemモデルとRoomモデルをインポート
-from .serializers import ItemSerializer, RoomSerializer, QuizData1Serializer # 同じディレクトリにあるserializers.pyファイルからItemSerializerとRoomSerializerをインポート
-from .models import User # 同じディレクトリにあるmodels.pyファイルからUserモデルをインポート
-from .serializers import UserSerializer # 同じディレクトリにあるserializers.pyファイルからUserSerializerをインポート
+from .serializers import ItemSerializer, UserSerializer, RoomSerializer, QuizData1Serializer # 同じディレクトリにあるserializers.pyファイルからItemSerializerとRoomSerializerをインポート
+from .models import Item, Room, User, QuizData1 # 同じディレクトリにあるmodels.pyファイルからItemモデルをインポート
 
 # Userモデルに対するCRUD（Create, Read, Update, Delete）操作を処理するUserViewSetクラスを定義
 # viewsets.ModelViewSetを継承している
@@ -26,6 +26,17 @@ class RoomViewSet(viewsets.ModelViewSet):
 class QuizData1ViewSet(viewsets.ModelViewSet):
     queryset = QuizData1.objects.all()
     serializer_class = QuizData1Serializer
+
+# ルームidが存在するかどうかを返す
+@require_GET
+def room_exists_api(request):
+    room_id = request.GET.get('roomId')
+
+    if not room_id:
+        return JsonResponse({ 'error': 'roomId parameter is required '}, status=404)
+
+    exists = Room.objects.filter(roomId=room_id).exists()
+    return JsonResponse({ 'exists': exists })
 
 # Create your views here.
 def index(request):
