a58528fc29e886d035c02501d37d465ba723c737
Aoyama <aoyama.sora.27s@gmail.com>
2025-07-13 09:31:31 +0900

    feat: testquestionに問題作成(#38)

--- main/management/commands/create_test_data.py
+++ main/management/commands/create_test_data.py
@@ -11,25 +11,41 @@ class Command(BaseCommand):
             {
                 'questionId': 0,
                 'quizId': 1,
-                'question': 'What is the Japanese word for apple?',
-                'answer_full': 'りんご',
-                'answer_letters': ['り', 'ん', 'ご'],
+                'question': '夏の大三角とは、こと座のベガ、わし座のアルタイル、はくちょう座の何？',
+                'answer_full': 'デネブ',
+                'answer_letters': ['で', 'ね', 'ぶ'],
                 'category': 'Japanese'
             },
             {
                 'questionId': 1,
                 'quizId': 1,
-                'question': 'What is the Japanese word for cat?',
-                'answer_full': 'ねこ',
-                'answer_letters': ['ね', 'こ'],
+                'question': '「夏草や 兵どもが 夢の中」という句を読んだ俳人は誰？',
+                'answer_full': '松尾芭蕉',
+                'answer_letters': ['ま', 'つ', 'お', 'ば', 'し', 'ょ', 'う'],
                 'category': 'Japanese'
             },
             {
                 'questionId': 2,
                 'quizId': 1,
-                'question': 'What is the Japanese word for water?',
-                'answer_full': 'みず',
-                'answer_letters': ['み', 'ず'],
+                'question': '毎年7月に行われ、京の三大祭の一つとしても知られる祭は何？',
+                'answer_full': '祇園祭',
+                'answer_letters': ['ぎ', 'お', 'ん', 'ま', 'つ', 'り'],
+                'category': 'Japanese'
+            },
+            {
+                'questionId': 3,
+                'quizId': 1,
+                'question': '13年周期や17年周期など周期的に大量発生するセミのことを、その周期の特徴から何と呼ぶ？',
+                'answer_full': '素数ゼミ',
+                'answer_letters': ['そ', 'す', 'う', 'ぜ', 'み'],
+                'category': 'Japanese'
+            },
+            {
+                'questionId': 4,
+                'quizId': 1,
+                'question': 'テングサという海藻を煮て作られ、漢字では「心太」と書かれるゼリー状の食品は何？',
+                'answer_full': 'ところてん',
+                'answer_letters': ['と', 'こ', 'ろ', 'て', 'ん'],
                 'category': 'Japanese'
             }
         ]
